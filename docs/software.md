841a75dc33d0956e958d269c58dc9dc9d2487433
Eric Li <ericljz@hotmail.com>
2020-06-06 21:21:45 +0800

    Add class AD5252 and Update Doc

--- docs/software.md
+++ docs/software.md
@@ -1,9 +1,50 @@
 软件设计
 =============
 ## AD5252.h
-AD5252是一款具有256位的双通道、I2C、非易失性存储器、数控电位计。可实现与机械电位计、调整器和可变电阻相同的电子调整功能。器件的多功能编程能力支持多种工作模式，包括对RDAC和EEMEM寄存器进行读/写访问、电阻递增/递减、±6 dB电阻量程变化和游标设置回读，同时提供额外的EEMEM，用于存储用户定义信息，如其他元件的存储器数据、查找表或系统识别信息。
+AD5252.h文件为我们自主编写的AD5252双通道数字电位器的控制相关库。其提供了设置AD5252电位器特定通道阻值、读取其特定通道阻值的功能。
+该头文件定义了AD5252类，该类下包含了5个函数：
+|函数|功能|
+|------|------|
+|AD5252.setR1(int)|设定R1电阻大小（输入范围：0～255）|
+|AD5252.setR2(int)|设定R2电阻大小（输入范围：0～255）|
+|AD5252.readR1()|读取R1电阻大小（输出范围：0～255）|
+|AD5252.readR2()|读取R2电阻大小（输出范围：0～255）|
+|AD5252.setup()|初始化与AD5252链接总线，并将电位器拨片置中|
 
-AD5252.h文件
+### 实现细节:
+AD5252是一款具有256位双通道、I2C、非易失性存储器的数控电位计。可实现与机械电位计、调整器和可变电阻相同的电子调整功能。
+
+
+我们使用I2C总线与AD5252芯片通信。在Arduino中，有Wire库提供I2C总线通讯的封装，我们利用这一库进行了AD5252相关代码的编写。
+
+### 实现源码:
+AD5252阻值设定：
+```c/c++
+void AD5252_setR(int channel,int R1){ //Set POT
+	Wire.beginTransmission(Addr); //Start Transmission
+	Wire.write(byte(channel)); //Send instruction for channel
+	Wire.write(R1); //send resistance value
+	Wire.endTransmission(); //Stop transmission
+}
+```
+
+AD5252阻值读取：
+```C/c++
+int AD5252_readR(int channel){ //Read resistance
+    Wire.beginTransmission(Addr);
+    Wire.write(byte(channel)); //Send instruction for channel
+    Wire.endTransmission();
+    Wire.requestFrom(Addr,1); //Request 1 byte of data
+
+    int data = -1; //If no return from chip,return -1
+    //Read 1 byte if data available 
+    if(Wire.available() == 1){
+        data = Wire.read();
+    }
+    return data;
+}
+```
+
 
 ## main.cpp
 
--- include/AD5252.h
+++ include/AD5252.h
@@ -3,46 +3,46 @@
 //AD5252 I2C adress is 0x2C(44)(0101100)
 #define Addr 0x2C
 
-void AD5252_setR(int channel,int R1){ //Set POT
+void AD5252_setR(int channel,int R){ //Set POT
     Wire.beginTransmission(Addr); //Start Transmission
     Wire.write(byte(channel)); //Send instruction for channel
-    Wire.write(R1); //send resistance value
+    Wire.write(R); //send resistance value
     Wire.endTransmission(); //Stop transmission
 }
 
-void AD5252_setR1(int R1){ //Set POT-1 resistance
+void AD5252::setR1(int R1){ //Set POT-1 resistance
     AD5252_setR(0x01,R1);
 }
 
-void AD5252_setR2(int R2){ //Set POT-3 resistance
+void AD5252::setR2(int R2){ //Set POT-3 resistance
     AD5252_setR(0x03,R2);
 }
 
 int AD5252_readR(int channel){ //Read resistance
     Wire.beginTransmission(Addr);
-    Wire.write(byte(channel));
+    Wire.write(byte(channel)); //Send instruction for channel
     Wire.endTransmission();
     Wire.requestFrom(Addr,1); //Request 1 byte of data
 
-    int data = -256;
-    //Read 1 byte if data
+    int data = -1; //If no return from chip, return -1
+    //Read 1 byte if data available
     if(Wire.available() == 1){
         data = Wire.read();
     }
     return data;
 }
 
-float AD5252_readR1(){ //Read POT-1 resistance
+float AD5252::readR1(){ //Read POT-1 resistance
     float R1 = AD5252_readR(0x01);
     return R1;
 }
 
-float AD5252_readR2(){ //Read POT-3 resistance
+float AD5252::readR2(){ //Read POT-3 resistance
     float R2 = AD5252_readR(0x03);
     return R2;
 }
 
-void AD5252_setup(){ //Setup AD5252
+void AD5252::setup(){ //Setup AD5252
     DDRB |= (1 << DDB0);
     PORTB |= (1 << PORTB0);
     Wire.begin();   //Initialise I2C communication as Master
